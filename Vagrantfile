5b1ad28bb5971eeed7cfd6520d549ccf1859346f
Bassinator <bastian.greuel@gmail.com>
2018-01-18 15:42:03 +0100

    major changes for helsana

--- Vagrantfile
+++ Vagrantfile
@@ -7,12 +7,55 @@
 # you're doing.
 Vagrant.configure("2") do |config|
 
-  config.vm.box = "ol74"
-  config.vm.hostname = 'odiDevEnv'
-  config.vm.synced_folder "~/Downloads", "/vagrant_downloads/"
+  class Username
+    def to_s
+      print "Virtual machine needs you ex or hk Helsana user and password.\n"
+      print "Username: "
+      STDIN.gets.chomp
+    end
+  end
 
-  config.vm.provision "ansible_local" do |ansible|
-    ansible.verbose = "vvv"
-    ansible.playbook = "devEnv.yml"
+  class Password
+    def to_s
+      begin
+      system 'stty -echo'
+      print "Password: "
+      pass = URI.escape(STDIN.gets.chomp)
+      ensure
+      system 'stty echo'
+      end
+      pass
+    end
+  end
+
+
+  config.vm.box = "ol74"
+
+  #following fixes a bug see https://github.com/hashicorp/vagrant/issues/7648
+  config.vm.provider 'virtualbox' do |vb|
+    vb.customize ['modifyvm', :id, '--cableconnected1', 'on']
+  end
+  #config.vm.hostname = 'odiDevEnv'
+  #config.vm.network "public_network", ip: "192.168.1.4"
+  #config.vm.network "private_network", ip: "192.168.50.4",
+  #  virtualbox__intnet: true
+  #config.vm.synced_folder "~/Downloads", "/vagrant_downloads/"
+  #config.vm.synced_folder "C:\\Users\\EX3NS\\Downloads", "/vagrant_downloads/"
+  config.vm.synced_folder 'U:\Team\ICC\8 Work\64-bit\VirtualBox', "/media/Virtualbox"
+  config.vm.synced_folder 'U:\Team\ICC\8 Work\DPF Upgrade\12.2.1', "/media/sf_DPF_Upgrade/12.2.1"
+
+  #TODO: replace with ruby see: https://github.com/hashicorp/vagrant/issues/2662
+  #config.vm.provision "shell" , path: "script.sh", :args => [Username.new, Password.new]
+  #config.vm.provision "shell" , path: "script.sh", env: {"USERNAME" => Username.new, "PASSWORD" => Password.new}
+  config.vm.provision "shell" , path: "script.sh", env: {"USERNAME" =>"ex3ns", "PASSWORD" => "ma3xi3Ap"}
+
+  config.vm.provision "ansible_local" do |ansible1|
+    ansible1.verbose = "vvv"
+    ansible1.playbook = "baseSetup.yml"
+  end
+
+  config.vm.provision "ansible_local" do |ansible2|
+    ansible2.verbose = "vvv"
+    ansible2.playbook = "devEnv.yml"
   end
 end
--- script.sh
+++ script.sh
@@ -1,12 +1,67 @@
+#!/bin/sh
+
+#USERNAME=$1
+#PASSWORD=$2
+
 setenforce Permissive
 sed --in-place s/SELINUX=enforcing/SELINUX=permissive/g /etc/selinux/config
 
 rpm --install /media/sf_DPF_Upgrade/12.2.1/cntlm-0.92.3-1.x86_64.rpm
-mkdir /var/run/cntlm
-chgrp cntlm /var/run/cntlm/
-chmod g+w /var/run/cntlm/
-usermod --home /home/cntlm --move-home cntlm
-sed --in-place s#'PIDFILE="/var/run/cntlm/cntlmd.pid"'#'PIDFILE="/tmp/cntlmd.pid"'#g /etc/sysconfig/cntlmd
+cp -u /vagrant/tmpfile.cntlm.conf /usr/lib/tmpfiles.d/cntlm.conf
+systemd-tmpfiles --create --remove
+#usermod --home /home/cntlm --move-home cntlm
+#sed --in-place s#'PIDFILE="/var/run/cntlm/cntlmd.pid"'#'PIDFILE="/tmp/cntlmd.pid"'#g /etc/sysconfig/cntlmd
+#yes | cp -rf /vagrant/cntlm.conf /etc/cntlm.conf
+#cat /vagrant/cntlmAuth.txt >> /etc/cntlm.conf
 
-yum install -y ansible
-yum install bind-utils
+grep -q '^Header' /etc/cntlm.conf &&
+  sed -i "/^Header/ c\Header  User-Agent: Mozilla/4.0 (compatible; MSIE 5.5; Windows 98)" /etc/cntlm.conf ||
+  echo "Header  User-Agent: Mozilla/4.0 (compatible; MSIE 5.5; Windows 98)" >> /etc/cntlm.conf;
+
+# add helsana rpm repo to noproxy urls
+grep -q '^NoProxy' /etc/cntlm.conf &&
+  sed -i "/^NoProxy/ c\NoProxy  localhost, 127.0.0.*, 10.*, 192.168.*, admksp01.hel.kko.ch" /etc/cntlm.conf ||
+  echo "NoProxy  localhost, 127.0.0.*, 10.*, 192.168.*, admksp01.hel.kko.ch" >> /etc/cntlm.conf;
+
+grep -q '^Proxy' /etc/cntlm.conf &&
+  sed -i "/^Proxy/ c\Proxy		proxy-surf:8080" /etc/cntlm.conf ||
+  echo "Proxy		proxy-surf:8080" >> /etc/cntlm.conf;
+
+grep -q '^Password' /etc/cntlm.conf &&
+  sed -i "/^Password/ c\#Password  top-secret" /etc/cntlm.conf
+
+grep -q '^Username' /etc/cntlm.conf &&
+  sed -i "/^Username/ c\Username  $USERNAME" /etc/cntlm.conf ||
+  echo "Username  $USERNAME" >> /etc/cntlm.conf;
+
+grep -q '^Domain' /etc/cntlm.conf &&
+  sed -i "/^Domain/ c\Domain  ads.hel.kko.ch" /etc/cntlm.conf ||
+  echo "Domain  ads.hel.kko.ch" >> /etc/cntlm.conf;
+
+
+echo $PASSWORD | cntlm -H -d ads.hel.kko.ch -u $USERNAME | while read line
+do
+  if [[ "$line" =~ ^PassNTLMv2.* ]]; then
+    grep -q '^PassNTLMv2' /etc/cntlm.conf &&
+      sed -i "/^PassNTLMv2/ c\\$line" /etc/cntlm.conf ||
+      echo "$line" >> /etc/cntlm.conf;
+fi
+done
+
+
+systemctl restart cntlm.service
+grep -q '^proxy=' /etc/yum.conf &&
+  sed -i '/^proxy=/ c\proxy=http://localhost:3128' /etc/yum.conf ||
+  echo "proxy=http://localhost:3128" >> /etc/yum.conf;
+
+
+# need to be done with yum behind proxy, because rpm -i xyz is not aware of proxy
+yes | yum -y install ansible
+
+# update base installation
+# yes | yum -y update
+
+
+
+
+#yes | yum install -y hel-rootca
